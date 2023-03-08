41fcc4aed84e5aa60d8a049c32107fdcd0bbb295
Andre Lima <andre.lima@viaconsultingway.com>
2023-03-08 14:22:17 +0000

    fix inputTel

--- public/phone.js
+++ public/phone.js
@@ -1,10 +1,8 @@
-
-
 var input = document.querySelector("#mobile_code");
 var inputInstance = intlTelInput(input, {
-	initialCountry: "pt",
-    nationalMode:true,
-	separateDialCode: true,
-    utilsScript:"inputTel/build/js/utils.js",
-    separateDialCode:true
+    initialCountry: "pt",
+    nationalMode: true,
+    separateDialCode: true,
+    utilsScript: "../inputTel/build/js/utils.js",
+    separateDialCode: true
 });
